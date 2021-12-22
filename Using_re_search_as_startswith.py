172ebc1ff4caec26f577c5b5ca13c1253354eec3
yueqi719 <yueqi_w719@hotmail.com>
2021-12-22 14:00:50 +0800

    add ^ to restrict that "From:" should be at the beginning of the line

--- Using_re_search_as_startswith.py
+++ Using_re_search_as_startswith.py
@@ -3,7 +3,7 @@ import re
 hand = open('mbox-short.txt')
 for line in hand:
     line = line.rstrip()
-    if re.search('From:',line):
+    if re.search('^From:',line):
         print(line)
 
 #for line in hand:
