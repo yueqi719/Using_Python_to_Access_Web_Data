1fe3f0e260f2bd8c92c2a62969e3006ca921e5be
yueqi719 <yueqi_w719@hotmail.com>
2022-01-03 21:24:20 +0800

    done with the assginment

---
+++ Extracting_Data_from_JSON.py
@@ -0,0 +1,30 @@
+#Extracting Data from JSON
+
+#In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/json2.py. 
+#The program will prompt for a URL, read the JSON data from that URL using urllib and 
+#then parse and extract the comment counts from the JSON data, compute the sum of the numbers 
+#in the file and enter the sum below:
+#We provide two files for this assignment. 
+#One is a sample file where we give you the sum for your testing and 
+#the other is the actual data you need to process for the assignment.
+
+#Sample data: http://py4e-data.dr-chuck.net/comments_42.json (Sum=2553)
+#Actual data: http://py4e-data.dr-chuck.net/comments_1426898.json (Sum ends with 33)
+#You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.
+import urllib.request, urllib.parse, urllib.error
+import json
+
+url = input('Enter location: ')
+if len(url) < 1:
+    url = 'http://py4e-data.dr-chuck.net/comments_1426898.json'
+print('Retrieving:', url)
+
+uh = urllib.request.urlopen(url)
+data= uh.read()
+
+sum = 0
+info = json.loads(data)
+for item in info["comments"]:
+	count = int(item["count"])
+	sum = sum + count
+print(sum)
